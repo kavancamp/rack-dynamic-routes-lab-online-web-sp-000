e2020053db0c6da0e1dd2c21c2693bee7d001bf4
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-12 19:41:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/items"
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
@@ -12,7 +12,7 @@ class Application
       resp.write "Route not found"
       resp.status = 400
     end
- 
+
     resp.finish
   end
 end
