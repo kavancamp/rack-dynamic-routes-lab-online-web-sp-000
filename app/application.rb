d0f365cce92da007a0c2b4dd28b415e40671958e
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-12 19:42:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,12 +7,16 @@ class Application
     if req.path=="/items"
       item_name = req.path.split("/items/").last
       if item =@@items.find{|i| i.name == item_name}
+
          resp.write "item.price"
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 400
     end
-
+  else
+   resp.status=404
+   resp.write "Route not found"
+ end
     resp.finish
   end
 end
