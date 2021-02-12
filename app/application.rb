37633ae6027408c07689ba95f35c334e832c6436
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-12 19:41:00 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/items"
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+         resp.write "item.price"
+    else
+      resp.write "Route not found"
+      resp.status = 400
+    end
+ 
+    resp.finish
+  end
+end
